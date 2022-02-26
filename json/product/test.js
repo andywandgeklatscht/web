e7fd3a30dc2a936ff0842507de4db4ed6ffba5a9
andywandgeklatscht <100236499+andywandgeklatscht@users.noreply.github.com>
2022-02-26 09:18:03 +0100

    Update test.js

--- json/product/test.js
+++ json/product/test.js
@@ -1,4 +1,15 @@
 {
   "productnr" : "0"
   "name" : "beispiel"
+  "kategorie": [
+    {
+      "0": "Lebensmittel", "1": "Handelsgüter", "2": "Baustoffe"
+    }
+  ]
+  "info": "kurzinformation"
+  "beschreibung": "artikelbeschreibung"
+  "preis": "1000€"
+  "menge": [
+    "100g", "500g", "1kg"
+  ]
 }
